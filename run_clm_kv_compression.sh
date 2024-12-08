8c42f615b9aeb9684616d92784972d185bc4db76
Joshua Robinson <robinson.josh.j@gmail.com>
2024-12-08 15:17:21 -0800

    Allow for more flexibility from command line

--- run_clm_kv_compression.sh
+++ run_clm_kv_compression.sh
@@ -1,5 +1,5 @@
 CUDA_VISIBLE_DEVICES=0, python run_clm_kv_compression.py \
-    --model_name_or_path togethercomputer/RedPajama-INCITE-Base-3B-v1 \
+    --model_name_or_path $1 \
     --train_file wikitext2/train.json \
     --validation_file wikitext2/test.json \
     --per_device_train_batch_size 4 \
@@ -7,10 +7,10 @@ CUDA_VISIBLE_DEVICES=0, python run_clm_kv_compression.py \
     --per_device_eval_batch_size 4 \
     --block_size 256 \
     --preprocessing_num_workers 12 \
-    --output_dir ./output/kv_${1} \
+    --output_dir ./output/${1}/kv_${2}_${3} \
     --compress \
-    --max_span_length 25 \
-    --bound_ratio $1 \
+    --max_span_length $3 \
+    --bound_ratio $2 \
     --r 16 \
     --seed 0 \
     --learning_rate 2e-5
--- run_clm_scattered_attention.sh
+++ run_clm_scattered_attention.sh
@@ -1,5 +1,5 @@
 CUDA_VISIBLE_DEVICES=0, python run_clm_scattered_attention.py \
-    --model_name_or_path togethercomputer/RedPajama-INCITE-Base-3B-v1 \
+    --model_name_or_path $1 \
     --train_file wikitext2/train.json \
     --validation_file wikitext2/test.json \
     --per_device_train_batch_size 4 \
@@ -7,9 +7,9 @@ CUDA_VISIBLE_DEVICES=0, python run_clm_scattered_attention.py \
     --per_device_eval_batch_size 4 \
     --block_size 256 \
     --preprocessing_num_workers 12 \
-    --output_dir ./output/scattered_${1} \
+    --output_dir ./output/${1}/scattered_${2} \
     --compress \
-    --bound_ratio $1 \
+    --bound_ratio $2 \
     --r 16 \
     --seed 0 \
     --learning_rate 2e-5
